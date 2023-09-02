d07bdbae10e27623bf224de6348856358eaa31b6
Tushar <tushar.jain@ottonomy.com>
2023-09-02 18:18:12 +0530

    first app react

---
+++ src/components/AddBook.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddBook = () => {
+  return (
+    <div>AddBook from here</div>
+  )
+}
+
+export default AddBook
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div>This isHome</div>
+  )
+}
+
+export default Home
